4e9c63c887165410ca3805748ce8315ac173c34b
DanAlarbash <dan.alarbash@gmail.com>
2017-07-24 19:45:05 +0300

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("post:detail", kwargs={"post_id":self.id})
+		return reverse("posts:detail", kwargs={"post_id":self.id})
 
 
 
