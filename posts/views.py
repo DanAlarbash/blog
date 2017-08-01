e9edb2eb3411eadeefcca8edd8674b8b9bc0bc69
Aldan <dan.alarbash@gmail.com>
2017-08-01 19:20:00 +0300

    Week 3

--- posts/views.py
+++ posts/views.py
@@ -2,16 +2,75 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . models import Post
 from django.shortcuts import get_object_or_404
-from .forms import PostForm
+from .forms import PostForm, UserSignUp, UserLogin
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from urllib.parse import quote
+from django.http import Http404
+from django.utils import timezone
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+
+
+def usersignup(request):
+	context = {}
+	form = UserSignUp()
+	context ['form'] = form 
+	if request.method == "POST":
+		form = UserSignUp(request.POST)
+		if form.is_valid():
+			user = form.save(commit=False)
+			username = user.username
+			password = user.password
+			user.set_password(password)
+			user.save()
+			auth_user = authenticate(username=username, password=password)
+			login(request, auth_user)
+
+			return redirect("posts:list")
+		messages.error(request, form.errors)
+		return redirect("posts:signup")
+	return render(request, 'signup.html', context)
+
+def userlogout(request):
+	logout(request)
+	return redirect("posts:login")
+
+
+def userlogin(request):
+	context = {}
+	form = UserLogin()
+	context['form'] = form
+	if request.method == "POST":
+		form = UserLogin(request.POST)
+		if form.is_valid():
+
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			auth_user = authenticate(username=username, password=password)
+			if auth_user is not None:
+				login(request, auth_user)
+				return redirect("posts:list")
+
+			message.warning(request, "Wrong username/password combination. Please try again.")
+			return redirect("posts:login")
+		messages.error(request, form.errors)
+		return redirect("posts:login")
+	return render(request, 'login.html', context)
 
 
 
 # Create your views here.
-def post_detail(request, post_id):
-	obj =  get_object_or_404(Post, id=post_id)
+def post_detail(request, slug):
+	obj =  get_object_or_404(Post, slug=slug)
+	date = timezone.now().date()
+
+	if obj.publish > date or obj.draft:
+		if not(request.user.is_staff or request.user.is_superuser):
+			raise Http404
+
+
 	context = {
 		"instance": obj,
 		#"share_string": quote(obj.content),
@@ -21,8 +80,23 @@ def post_detail(request, post_id):
 
 
 def post_list(request):
-	obj_list = Post.objects.all()#.order_by("-timestamp","title")
-	paginator = Paginator(obj_list, 3) # Show 8 contacts per page
+	today = timezone.now().date()
+	
+	if request.user.is_staff or request.user.is_superuser:
+		obj_list = Post.objects.all()
+	else:
+		obj_list = Post.objects.filter(draft=False).filter(publish__lte=today)
+
+	query = request.GET.get("q")
+	if query:
+		obj_list = obj_list.filter(
+			Q(title__icontains=query)| #contains if case sensative
+			Q(content__icontains=query)|
+			Q(author__first_name__icontains=query)|
+			Q(author__last_name__icontains=query)
+			).distinct()
+
+	paginator = Paginator(obj_list, 4) # Show 8 contacts per page
 	page = request.GET.get('page')
 	try:
 		objs = paginator.page(page)
@@ -35,13 +109,18 @@ def post_list(request):
 
 	context = {
 		"post_list": objs,
+		"today": today,
 	}
 	return render(request, 'post_list.html', context)
 
 def post_create(request):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
-		form.save()
+		obj = form.save(commit=False)
+		obj.author = request.user
+		obj.save()
 		messages.success(request, "OMG! So cool! You created an object.")
 		return redirect("posts:list")
 	context = {
@@ -50,8 +129,10 @@ def post_create(request):
 	return render(request, 'post_create.html', context)
 
 
-def post_update(request, post_id):
-	post_object = get_object_or_404(Post, id=post_id)
+def post_update(request, slug):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
+	post_object = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
@@ -64,7 +145,9 @@ def post_update(request, post_id):
 	return render(request, 'post_update.html', context)
 
 
-def post_delete(request, post_id):
-	obj = Post.objects.get(id=post_id).delete()
+def post_delete(request, slug):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
+	obj = Post.objects.get(slug=slug).delete()
 	messages.warning(request, "Seriosly bro?")
 	return redirect("posts:list")
